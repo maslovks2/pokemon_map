cbd8a918a15c0d5a9749d52f747a13c1a831dd51
maslovks2 <87320716+maslovks2@users.noreply.github.com>
2022-01-25 01:21:42 +0300

    add model PokemonEntity

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -8,3 +8,10 @@ class Pokemon(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class PokemonEntity(models.Model):
+
+    Lat = models.FloatField()
+    Lon = models.FloatField()
+    
